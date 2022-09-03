1397ae1173c530e38c6fd6d4ca714c94cd849462
lisnajose05 <112387580+lisnajose05@users.noreply.github.com>
2022-09-03 15:21:16 +0530

    Add files via upload

---
+++ SBA/Week1/Demo.java
@@ -0,0 +1,39 @@
+package com.ust;
+
+ class Employee {
+	int empid;
+	String name;
+	String designation;
+	float salary;
+	
+	float salary(float a) {
+		return a;
+	}
+	int salary(int a) {
+		return a;
+	}
+	
+
+	public  void display() {
+				System.out.println("in super class");
+			}
+	}
+ 
+	class Engineer extends Employee{
+		public void display() {
+			System.out.println("in sub class");
+			super.display();
+			
+		}
+	}
+		class Demo{
+	public static void main(String args[])
+	{
+		Engineer e=new Engineer();
+		Employee e1=new Employee();
+		System.out.println(e.salary(25000));
+		System.out.println(e.salary(35000f));
+		e.display();
+		e1.display();
+	}
+	}
