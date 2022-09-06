cca20dd13e0a770b2799610b8f4251f68161955f
lisnajose05 <112387580+lisnajose05@users.noreply.github.com>
2022-09-06 12:33:48 +0530

    Add files via upload

---
+++ JAVA/Method_overloading.java
@@ -0,0 +1,14 @@
+package poly.ust;
+
+public class Method_overloading {
+	private static void display(int a) {
+		System.out.println("Got Integer data.");
+	}
+private static void display(String a) {
+	System.out.println("Got String object");
+}
+public static void main(String args[]) {
+	display(1);
+	display("Hello");
+}
+}
---
+++ JAVA/Protected.java
@@ -0,0 +1,21 @@
+package poly.ust;
+class Animal{
+	protected String name;
+	protected void display() {
+		System.out.println("I am an animal");
+	}
+}
+class Dog extends Animal{
+	public void getInfo() {
+		System.out.println("My name is"+name);
+	}
+}
+public class Protected {
+	public static void main(String args[]) {
+		Dog labrador=new Dog();
+		labrador.name="Rocky";
+		labrador.display();
+		labrador.getInfo();
+	}
+
+}
