53cb9827bbd8ba8ae4b91312da5ee7a45f206e02
lisnajose05 <112387580+lisnajose05@users.noreply.github.com>
2022-09-07 16:43:11 +0530

    Add files via upload

---
+++ JAVA/Generics1.java
@@ -0,0 +1,25 @@
+package collection.ust;
+
+public class Generics1<T> {
+	T t;
+	public T get() {
+		return t;
+	}
+	
+	
+	public void set(T t) {
+		this.t=t;
+	}
+	public static void main(String args[]) {
+		Generics1<String> g=new Generics1<>();
+		g.set("Hi");
+		System.out.println(g.get());
+		
+		Generics1<Integer> gi=new Generics1<>();
+		gi.set(100);
+		System.out.println(gi.get());
+		
+		
+	}
+
+}
---
+++ JAVA/Map3.java
@@ -0,0 +1,17 @@
+package collection.ust;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class Map3 {
+	public static void main(String args[]) {
+		Map<Integer,String> m = new HashMap<>();
+		m.put(1,"S");
+		m.put(2,"A");
+		m.put(3,"f");
+		
+		Set keys =  m.keySet();
+		System.out.println(keys);
+	}
+
+}
