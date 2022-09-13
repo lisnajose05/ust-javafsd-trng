0cfb18b9a09187bd3b071f9f46772c59c060c075
lisnajose05 <112387580+lisnajose05@users.noreply.github.com>
2022-09-13 17:41:43 +0530

    Add files via upload

---
+++ JAVA/Enum_1.java
@@ -0,0 +1,12 @@
+package enu.ust;
+enum Size {
+	SMALL,MEDIUM,LARGE,EXTRALARGE
+	}
+
+public class Enum_1 {
+	public static void main(String args[]) {
+		System.out.println(Size.SMALL);
+		System.out.println(Size.MEDIUM);
+	}
+
+}
---
+++ JAVA/Enum_2.java
@@ -0,0 +1,36 @@
+package enu.ust;
+
+enum Size{
+
+SMALL,MEDIUM,LARGE,EXTRALARGE
+}
+class Test{
+	Size pizzaSize;
+	public Test(Size pizzaSize) {
+		this.pizzaSize=pizzaSize;
+	}
+	public void orderPizza() {
+		switch(pizzaSize) {
+		case SMALL:
+			System.out.println("I ordered a small size pizza");
+			break;
+		case MEDIUM:
+			System.out.println("I Ordered a medium size pizza");
+			break;
+		default:
+			System.out.println("I Ordered a medium size pizza");
+			break;
+			
+		}
+	}
+}
+public class Enum_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Test t1=new Test(Size.MEDIUM);
+		t1.orderPizza();
+
+	}
+
+}
